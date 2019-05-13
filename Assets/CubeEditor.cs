0f0411902e9853c5600d0d22c4258240b8d4ca40
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-13 14:18:43 -0400

    Adding label logic to the cube

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
-   [Range(1f,10f)] [SerializeField] float gridSise = 10f;
+    [Range(1f,10f)] [SerializeField] float gridSise = 10f;
+    TextMesh textMesh;
 
+    void Start()
+    {
+
+
+    }
     void Update()
     {
         Vector3 snapPos;
+
         float roundXPos = Mathf.RoundToInt(transform.position.x / gridSise);
         snapPos.x = roundXPos * 10f;
 
         float roundZPos = Mathf.RoundToInt(transform.position.z / gridSise);
         snapPos.z = roundZPos * 10f;
 
-       // Debug.Log("transform.position.x: " + transform.position.x + "roundPos: " + roundPos + " - snapPos.x : " + snapPos.x);
-
-        //snapPos.z = 0f;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x + "," + snapPos.z;
+
+       
 
         Debug.Log("Editor causes this Update");
     }
