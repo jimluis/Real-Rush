066c85a25a1f3c4d52772906b135bf2060736949
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-13 13:35:12 -0400

    Initial commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+   [Range(1f,10f)] [SerializeField] float gridSise = 10f;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        float roundXPos = Mathf.RoundToInt(transform.position.x / gridSise);
+        snapPos.x = roundXPos * 10f;
+
+        float roundZPos = Mathf.RoundToInt(transform.position.z / gridSise);
+        snapPos.z = roundZPos * 10f;
+
+       // Debug.Log("transform.position.x: " + transform.position.x + "roundPos: " + roundPos + " - snapPos.x : " + snapPos.x);
+
+        //snapPos.z = 0f;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        Debug.Log("Editor causes this Update");
+    }
+}
