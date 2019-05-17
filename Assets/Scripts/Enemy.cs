3dbb3ff5ba8e379a5b95f677c040112bd96102ec
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-17 12:16:14 -0400

    Adding particle effects to the enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,13 +6,24 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] int health = 20;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
 
     void OnParticleCollision(GameObject other)
     {
         if (health == 0)
+        {
+            var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+            vfx.Play();
+           // deathParticlePrefab.Play();
             Destroy(gameObject);
+        }
+
         else
+        { 
             health--;
+            hitParticlePrefab.Play();
+        }
 
         print("Enemy Hit - Health: ");
     }
