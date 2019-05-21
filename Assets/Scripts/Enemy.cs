016a6979be989424b6832db4b163ab38c1d30369
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-21 13:59:52 -0400

    Adding particle effects to enemy on arrival to base

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,19 +7,13 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] int health = 20;
     [SerializeField] ParticleSystem hitParticlePrefab;
-    [SerializeField] ParticleSystem deathParticlePrefab;
+    [SerializeField] ParticleSystem goalParticlePrefab;
 
     void OnParticleCollision(GameObject other)
     {
         if (health == 0)
         {
-            var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
-            vfx.Play();
-            float destroyDelay = vfx.main.duration;
-            print("destroyDelay: "+ destroyDelay);
-
-            Destroy(vfx.gameObject, destroyDelay);
-            Destroy(gameObject);
+            DestroyEnemy();
         }
 
         else
@@ -31,4 +25,14 @@ public class Enemy : MonoBehaviour
 
     }
 
+    public void DestroyEnemy()
+    {
+        var vfx = Instantiate(goalParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
+        float destroyDelay = vfx.main.duration;
+        print("destroyDelay: " + destroyDelay);
+
+        Destroy(vfx.gameObject, destroyDelay);
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [Range(0.1f, 120f)]
-    [SerializeField] float secondsBetweenSpawns = 4f;
+    [SerializeField] float secondsBetweenSpawns = 0.5f;
     [SerializeField] EnemyMovement enemyMovement;
     [SerializeField] Transform enemyParentTransform;
     void Start()
